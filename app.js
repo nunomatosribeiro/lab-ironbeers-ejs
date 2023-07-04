2134790dbeabb0cc12e13ef3a23e8162e07010be
unknown <nunoribeiroft@outlook.com>
2023-07-04 21:55:28 +0100

    done

--- app.js
+++ app.js
@@ -19,4 +19,35 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { data: beersFromApi });
+      console.log('Beers from the database: ', beersFromApi);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random_beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random_beer', { beer: beersFromApi[0] });
+      console.log('Random beer from the database:', beersFromApi[0]);
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const beerId = req.params.id;
+
+  punkAPI
+    .getBeer(beerId)
+    .then(beerArray => {
+      res.render('partials/beer', { beer: beerArray[0] });
+      console.log('Beer details:', beerArray[0]);
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
